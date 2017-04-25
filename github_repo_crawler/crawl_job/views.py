d809f919cca75af7a2224dd28f3dbd684fa8bdf2
Ubuntu <ubuntu@ip-172-31-46-154.us-west-2.compute.internal>
2017-04-25 03:47:17 +0000

    Deploy docker image to AWS Elastic Beanstalk

--- github_repo_crawler/crawl_job/views.py
+++ github_repo_crawler/crawl_job/views.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import logging
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
 
+logger = logging.getLogger("worker")
+
 def add_crawl_point(request):
+    logger.info("I am in crawl function")
     return HttpResponse("result")
