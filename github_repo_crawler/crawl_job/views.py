36be8106f6a31d7e8cbfcbedcb007ddfc662c610
Ubuntu <ubuntu@ip-172-31-46-154.us-west-2.compute.internal>
2017-04-25 04:07:30 +0000

    Can curl and get repo data

--- github_repo_crawler/crawl_job/views.py
+++ github_repo_crawler/crawl_job/views.py
@@ -3,12 +3,22 @@ from __future__ import unicode_literals
 
 import logging
 
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from github_api import *
 
 logger = logging.getLogger("worker")
 
 def add_crawl_point(request):
-    logger.info("I am in crawl function")
-    return HttpResponse("result")
+    logger.info("I am in crawl function ")
+    request_obj = json.loads(request.body)
+    print request_obj
+    name = request_obj["username"]
+    data = extract_data(name) # TODO: change parameter to request_obj
+    
+    logger.info("Result: {0}".format(json.dumps(data, indent=2)))
+
+    return HttpResponse(json.dumps(data, indent=2))
+
