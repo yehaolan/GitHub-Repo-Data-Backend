b5a853e3f8d5c1602b745ec0605ec61e35ad9035
Ubuntu <ubuntu@ip-172-31-46-154.us-west-2.compute.internal>
2017-04-26 16:39:41 +0000

    Create the new database

--- github_repo_crawler/crawl_job/db.sql
+++ github_repo_crawler/crawl_job/db.sql
@@ -1,17 +1,8 @@
--- how I create table in database
 CREATE TABLE GITHUB_REPO (
-    username             TEXT,
-    user_image           TEXT,
-    repo_id              INT,
-    repo_name            TEXT,
-    repo_description     TEXT,
-    repo_star            INT,
-    PRIMARY KEY (repo_id)
+    username    TEXT,
+    time        DOUBLE PRECISION,
+    PRIMARY KEY (username)
 );
 
-CREATE INDEX usernamex          ON GITHUB_REPO (username);
-CREATE INDEX user_imagex        ON GITHUB_REPO (user_image);
-CREATE INDEX repo_idx           ON GITHUB_REPO (repo_id);
-CREATE INDEX repo_namex         ON GITHUB_REPO (repo_name);
-CREATE INDEX repo_descriptionx  ON GITHUB_REPO (repo_description);
-CREATE INDEX repo_starx         ON GITHUB_REPO (repo_star);
+CREATE INDEX usernamex   ON GITHUB_REPO (username);
+CREATE INDEX timex   ON GITHUB_REPO (time);
